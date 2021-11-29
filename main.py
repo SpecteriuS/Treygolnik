654dd69a072f2402423c8ece1df4d427822e81fe
SpecteriuS <91021847+SpecteriuS@users.noreply.github.com>
2021-11-29 13:47:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 a=[]
 for i in range(1,51):
     a.append(i)
@@ -9,14 +8,14 @@ for x in range(0,48):
     if a[x]+a[x+1] > a[x+2] and a[x]< a[x+1]+a[x+2] and -a[x]+a[x+1] < a[x+2]:
         q=(a[x]+a[x+1]+a[x+2])/2
         s=(q*(q-a[x])*(q-a[x+1])*(q-a[x+2]))**0.5
-if s > smax:
-smax=s
-b1=a[x]
-b2=a[x+1]
-b3=a[x+2]
-break
+    if s > smax:
+        smax=s
+        b1=a[x]
+        b2=a[x+1]
+        b3=a[x+2]
+    break
 if smax==0:
-print("Er")
+    print("Er")
 else:
-print(smax)
-print(b1,b2,b3)
+    print(smax)
+    print(b1,b2,b3)
